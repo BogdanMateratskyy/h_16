4a01f8d5794043b633f9c77ac2f171da08ee9eed
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2018-12-12 15:53:32 +0200

    Added some methods and variables

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,10 +1,29 @@
 package cmd
+
+import People.Person
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println ("Hello, World!")
     println(s"Command line arguments: ${args.toList}")
     println(s"Grouped command line arguments: ${args.toList.grouped(2).toList}")
 
-    
+    val people = args.grouped(2).map { pair =>
+      val name = pair(0)
+      val age = pair(1).toInt
+
+      Person(name, age)
+    }.toList
+
+    val kids = people.filter(p => p.age < 12)
+    val elders = people.filter(p => p.age > 60)
+
+    println(s"People: ${people}")
+    println(s"Kids: ${kids}")
+    println(s"Elders: ${elders}")
+
+    val byAge = people.sortBy(_.age)
+
+    println(s"People sorted by age: ${byAge}")
   }
 }
